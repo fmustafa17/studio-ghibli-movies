607dfb319a97a825c91d4b1630a46f8e88120df6
Farhana Mustafa <farhana_mustafa@intuit.com>
2022-02-05 17:45:25 -0500

    add comments and remove unneeded code

--- Studio Ghibli API/View/ViewController.swift
+++ Studio Ghibli API/View/ViewController.swift
@@ -27,12 +27,14 @@ class ViewController: UITableViewController {
         bindViewModel()
     }
 
+    /// Register the cell for reuse and add any configurations for the tableView
     private func setUpTableView() {
         tableView.register(MovieTableCell.self, forCellReuseIdentifier: MovieTableCell.identifier)
         tableView.rowHeight = UITableView.automaticDimension
         tableView.estimatedRowHeight = 100.0
     }
 
+    /// Use the viewModel to fetch the data and publish the event
     private func bindViewModel() {
         self.viewModel.fetchMovies()
 
@@ -44,6 +46,7 @@ class ViewController: UITableViewController {
            .store(in: &cancellables)
     }
 
+    // MARK: - UITableViewDataSource
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return self.viewModel.movies.count
     }
@@ -51,10 +54,6 @@ class ViewController: UITableViewController {
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = (tableView.dequeueReusableCell(withIdentifier: MovieTableCell.identifier, for: indexPath) as? MovieTableCell)!
         cell.updateData(model: self.viewModel.movies[indexPath.row])
-
-//        let post = posts[indexPath.row]
-//        let isExpanded = expandedRow?.indexPath == indexPath
-//        cell.configure(expanded: isExpanded, post: post)
 
         // if the cell is expanded
         if expandedCellSet.contains(indexPath.row) {
@@ -72,16 +71,7 @@ class ViewController: UITableViewController {
         return cell
     }
 
-    // TODO: see how i can use this with combine or remove
-//    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-//        if editingStyle == .delete {
-//            tableView.beginUpdates()
-//            self.viewModel.movies.remove(at: indexPath.row)
-//            tableView.deleteRows(at: [indexPath], with: .top)
-//            tableView.endUpdates()
-//        }
-//    }
-    
+    // MARK: - UITableViewDelegate
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         tableView.deselectRow(at: indexPath, animated: false)
 
@@ -93,68 +83,7 @@ class ViewController: UITableViewController {
             expandedCellSet.insert(indexPath.row)
         }
 
-        // this will call cellForRowAt to update the cell's image and detailLabel
+        // this will call cellForRowAt to update the cell's movie title and description
         tableView.reloadData()
-
-//        tableView.beginUpdates()
-//        tableView.endUpdates()
     }
-
-    //    var expandedRow: (indexPath: IndexPath, height: CGFloat)? = nil
-    //    var collapsingRow: (indexPath: IndexPath, height: CGFloat)? = nil
-    //
-    //
-    //        override func numberOfSections(in tableView: UITableView) -> Int {
-    //            return 1
-    //        }
-    //
-    //
-    //
-    //        override func tableView(_ tableView: UITableView,
-    //                                heightForRowAt indexPath: IndexPath) -> CGFloat {
-    //            if indexPath == collapsingRow?.indexPath {
-    //                return collapsingRow!.height
-    //            } else {
-    //                return UITableView.automaticDimension
-    //            }
-    //        }
-    //
-    //        override func tableView(_ tableView: UITableView,
-    //                                didSelectRowAt indexPath: IndexPath) {
-    //            guard let tappedCell = tableView.cellForRow(at: indexPath) as? MyTableViewCell
-    //                else { return }
-    //
-    //            CATransaction.begin()
-    //            tableView.beginUpdates()
-    //
-    //            if let expandedRow = expandedRow,
-    //                let prevCell = tableView.cellForRow(at: expandedRow.indexPath)
-    //                    as? MyTableViewCell {
-    //                prevCell.heightConstraint.constant = prevCell.stackView.frame.height
-    //                prevCell.heightConstraint.isActive = true
-    //
-    //                CATransaction.setCompletionBlock {
-    //                    if let cell = tableView.cellForRow(at: expandedRow.indexPath)
-    //                        as? MyTableViewCell {
-    //                        cell.configureExpansion(false)
-    //                        cell.heightConstraint.isActive = false
-    //                    }
-    //                    self.collapsingRow = nil
-    //                }
-    //
-    //                collapsingRow = expandedRow
-    //            }
-    //
-    //
-    //            if expandedRow?.indexPath == indexPath {
-    //                collapsingRow = expandedRow
-    //                expandedRow = nil
-    //            } else {
-    //                tappedCell.configureExpansion(true)
-    //                expandedRow = (indexPath: indexPath, height: tappedCell.frame.height)
-    //            }
-    //
-    //            tableView.endUpdates()
-    //            CATransaction.commit()
-    //        }
 }
